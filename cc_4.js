fa7bc5ae227beb187fd2ea040bb8eaab58dc5573
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-18 08:23:57 -0400

    Create EV class extending Car with additional charge property.

---
+++ cc_4.js
@@ -0,0 +1,36 @@
+// Hoang Nguyen
+// U12840485
+
+class Car{
+    // Create the instances of the Car class with 'make' and 'speed' properties
+    constructor (make, speed){
+        this.make = make;
+        this.speed = speed;       
+    }
+
+    // Create the accelerate method 
+    accelerate (){
+        // Increase the speed by 10 each time it is called
+        this.speed += 10;
+
+        // Display the message
+        console.log(`The speed of the ${this.make} after accelerating is ${this.speed} km/h.`);
+    }
+
+    // Create the brake method 
+    brake () {
+        // Decrease the speed by 5 each time it is called
+        this.speed -= 5;
+
+        // Display the message
+        console.log(`The speed of the ${this.make} after braking is ${this.speed} km/h.`);
+    }
+}
+
+// Create an inheritance class EV
+class EV extends Car {
+    constructor (make, speed, charge){
+        super (make, speed);   // Parent class constructor with properties 'make' and 'speed
+        this.charge = charge;  // Initiate the instance of the EV class with property 'charge
+    }
+}
