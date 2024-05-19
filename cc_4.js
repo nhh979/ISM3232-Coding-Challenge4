1c3b69513207c5ac3bc403afd724b0db27bc9ad0
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-19 09:37:01 -0400

    Test EV class methods with different operations.

--- cc_4.js
+++ cc_4.js
@@ -41,11 +41,23 @@ class EV extends Car {
 
     accelerate () {
         this.speed += 20;    // Increase the speed by 20 
-        this.charge -= 1;   // Decrease the charge by 1%
+        this.charge -= 1;   // Decrease the charge by 1
 
         // Display the message
         console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%.`);
     }
 }
 
+let tesla = new EV("Tesla", 120, 23)
 
+console.log(`A ${tesla.make} starting at ${tesla.speed} km/h with a battery charge of ${tesla.charge}%.`);
+tesla.chargeBattery(10)
+console.log(`New battery charge: ${tesla.charge}%.`);
+tesla.accelerate();
+tesla.brake();
+tesla.accelerate();
+tesla.accelerate();
+
+// The battery charge decreases by 1% each acceleration. At some points, the charge could reach 0%, so further acceleration would not
+// theoretically possible. We need to consider the case when the charge is 0% to ensure that there would be no negative value for charge, and 
+// accelerattion is not allowed.
